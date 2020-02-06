708703f0e037c7063046a062568caf7ecc08ee32
Perfare <hjyqwert@126.com>
2020-02-06 21:39:01 +0800

    准备工作

--- Il2CppDumper/ElfConstants.cs
+++ Il2CppDumper/ElfConstants.cs
@@ -3,14 +3,19 @@
     static class ElfConstants
     {
         public const int DT_PLTGOT = 3;
+        public const int DT_HASH = 4;
         public const int DT_STRTAB = 5;
         public const int DT_SYMTAB = 6;
         public const int DT_RELA = 7;
         public const int DT_RELASZ = 8;
+        public const int DT_RELAENT = 9;
+        public const int DT_SYMENT = 11;
         public const int DT_INIT = 12;
         public const int DT_REL = 17;
         public const int DT_RELSZ = 18;
+        public const int DT_JMPREL = 23;
         public const int DT_INIT_ARRAY = 25;
+        public const int DT_FINI_ARRAY = 26;
         public const int DT_INIT_ARRAYSZ = 27;
 
         public const int R_ARM_ABS32 = 2;

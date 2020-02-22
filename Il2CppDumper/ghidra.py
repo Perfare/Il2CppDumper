36c4adb19d25483afe9021b5f0355b52d3079e4e
Perfare <hjyqwert@126.com>
2020-02-23 00:31:58 +0800

    调整ghidra脚本

--- Il2CppDumper/ghidra.py
+++ Il2CppDumper/ghidra.py
@@ -1,58 +1,61 @@
 # -*- coding: utf-8 -*-
-import ghidra.program.model.symbol.SourceType
-import re
+import json
 
 functionManager = currentProgram.getFunctionManager()
 baseAddress = currentProgram.getImageBase()
 USER_DEFINED = ghidra.program.model.symbol.SourceType.USER_DEFINED
+
+def get_addr(addr):
+	return baseAddress.add(addr)
+
+def set_name(addr, name):
+	name = name.replace(' ', '-')
+	createLabel(addr, name, True, USER_DEFINED)
+
+def make_function(start, end):
+	next_func_start = getFunctionAfter(start).getEntryPoint()
+	if next_func_start < end:
+		end = next_func_start
+	body = createAddressSet()
+	body.addRange(start, end.subtract(1))
+	functionManager.deleteAddressRange(start, end.subtract(1), getMonitor())
+	func = getFunctionAt(start)
+	if func is None:
+		functionManager.createFunction(None, start, body, USER_DEFINED)
+	else:
+		func.setBody(body)
+
+f = askFile("script.json from Il2cppdumper", "Open")
+data = json.loads(open(f.absolutePath, 'rb').read().decode('utf-8'))
+scriptMethods = data["ScriptMethod"]
+for scriptMethod in scriptMethods:
+	addr = get_addr(scriptMethod["Address"])
+	name = scriptMethod["Name"].encode("utf-8")
+	set_name(addr, name)
 index = 1
-
-def _convert_arg_addr(arg):
-    return baseAddress.add(int(arg, 0))
-
-def _convert_arg_string(arg):
-    if arg.startswith('r'):
-        return arg[2:-1]
-    return arg[1:-1]
-
-def do_idc_set_cmt(arg1, arg2):
-    addr = _convert_arg_addr(arg1)
-    text = _convert_arg_string(arg2)
-    setEOLComment(addr, text)
-
-def do_SetName(arg1, arg2):
-    addr = _convert_arg_addr(arg1)
-    name = _convert_arg_string(arg2)
-    createLabel(addr, name, True, USER_DEFINED)
-
-def do_SetString(arg1, arg2):
-    addr = _convert_arg_addr(arg1)
-    text = _convert_arg_string(arg2)
-    
-    global index
-    name = "StringLiteral_" + str(index);
-    createLabel(addr, name, True, USER_DEFINED)
-    setEOLComment(addr, text)
-    index += 1
-
-def do_MakeFunction(arg1, arg2):
-    start = _convert_arg_addr(arg1)
-    end = _convert_arg_addr(arg2)
-    next_func_start = getFunctionAfter(start).getEntryPoint()
-    if next_func_start < end:
-        end = next_func_start
-    body = createAddressSet()
-    body.addRange(start, end.subtract(1))
-    functionManager.deleteAddressRange(start, end.subtract(1), getMonitor())
-    func = getFunctionAt(start)
-    if func is None:
-        functionManager.createFunction(None, start, body, USER_DEFINED)
-    else:
-        func.setBody(body)
-
-f = askFile("ida.py from Il2cppdumper", "Open")
-for line in file(f.absolutePath):
-    match = re.search(r"^([\w+\.]+)\((\w+),\s*(.*)\)$", line)
-    if match:
-        name, arg1, arg2 = match.groups()
-        res = globals()['do_'+name.replace('.', '_')](arg1, arg2.replace(' ', '-'))
+scriptStrings = data["ScriptString"]
+for scriptString in scriptStrings:
+	addr = get_addr(scriptString["Address"])
+	value = scriptString["Value"].encode("utf-8")
+	name = "StringLiteral_" + str(index)
+	createLabel(addr, name, True, USER_DEFINED)
+	setEOLComment(addr, value)
+	index += 1
+scriptMetadatas = data["ScriptMetadata"]
+for scriptMetadata in scriptMetadatas:
+	addr = get_addr(scriptMetadata["Address"])
+	name = scriptMetadata["Name"].encode("utf-8")
+	set_name(addr, name)
+	setEOLComment(addr, name)
+scriptMetadataMethods = data["ScriptMetadataMethod"]
+for scriptMetadataMethod in scriptMetadataMethods:
+	addr = get_addr(scriptMetadataMethod["Address"])
+	name = scriptMetadataMethod["Name"].encode("utf-8")
+	methodAddr = get_addr(scriptMetadataMethod["MethodAddress"])
+	set_name(addr, name)
+	setEOLComment(addr, name)
+addresses = data["Addresses"]
+for index in range(len(addresses) - 1):
+	start = get_addr(addresses[index])
+	end = get_addr(addresses[index + 1])
+	make_function(start, end)

3ff284ee2dedc02bfb2df50e4ae45a1f003d5b36
sskaje <sskaje@gmail.com>
2020-06-02 23:12:53 +0800

    Python 脚本导入字段可配置 (#288)
    * Python: config array for script fields
    
    * fix indention

--- Il2CppDumper/ghidra.py
+++ Il2CppDumper/ghidra.py
@@ -1,6 +1,14 @@
 # -*- coding: utf-8 -*-
 import json
 
+processFields = [
+	"ScriptMethod",
+	"ScriptString",
+	"ScriptMetadata",
+	"ScriptMetadataMethod",
+	"Addresses",
+]
+
 functionManager = currentProgram.getFunctionManager()
 baseAddress = currentProgram.getImageBase()
 USER_DEFINED = ghidra.program.model.symbol.SourceType.USER_DEFINED
@@ -28,36 +36,48 @@ def make_function(start, end):
 print 'Script: Edit config.json and set MakeFunction to false to speed up ghidra script execution'
 f = askFile("script.json from Il2cppdumper", "Open")
 data = json.loads(open(f.absolutePath, 'rb').read().decode('utf-8'))
-scriptMethods = data["ScriptMethod"]
-for scriptMethod in scriptMethods:
-	addr = get_addr(scriptMethod["Address"])
-	name = scriptMethod["Name"].encode("utf-8")
-	set_name(addr, name)
-index = 1
-scriptStrings = data["ScriptString"]
-for scriptString in scriptStrings:
-	addr = get_addr(scriptString["Address"])
-	value = scriptString["Value"].encode("utf-8")
-	name = "StringLiteral_" + str(index)
-	createLabel(addr, name, True, USER_DEFINED)
-	setEOLComment(addr, value)
-	index += 1
-scriptMetadatas = data["ScriptMetadata"]
-for scriptMetadata in scriptMetadatas:
-	addr = get_addr(scriptMetadata["Address"])
-	name = scriptMetadata["Name"].encode("utf-8")
-	set_name(addr, name)
-	setEOLComment(addr, name)
-scriptMetadataMethods = data["ScriptMetadataMethod"]
-for scriptMetadataMethod in scriptMetadataMethods:
-	addr = get_addr(scriptMetadataMethod["Address"])
-	name = scriptMetadataMethod["Name"].encode("utf-8")
-	methodAddr = get_addr(scriptMetadataMethod["MethodAddress"])
-	set_name(addr, name)
-	setEOLComment(addr, name)
-addresses = data["Addresses"]
-for index in range(len(addresses) - 1):
-	start = get_addr(addresses[index])
-	end = get_addr(addresses[index + 1])
-	make_function(start, end)
+
+if "ScriptMethod" in data and "ScriptMethod" in processFields:
+	scriptMethods = data["ScriptMethod"]
+	for scriptMethod in scriptMethods:
+		addr = get_addr(scriptMethod["Address"])
+		name = scriptMethod["Name"].encode("utf-8")
+		set_name(addr, name)
+
+if "ScriptString" in data and "ScriptString" in processFields:
+	index = 1
+	scriptStrings = data["ScriptString"]
+	for scriptString in scriptStrings:
+		addr = get_addr(scriptString["Address"])
+		value = scriptString["Value"].encode("utf-8")
+		name = "StringLiteral_" + str(index)
+		createLabel(addr, name, True, USER_DEFINED)
+		setEOLComment(addr, value)
+		index += 1
+
+if "ScriptMetadata" in data and "ScriptMetadata" in processFields:
+	scriptMetadatas = data["ScriptMetadata"]
+	for scriptMetadata in scriptMetadatas:
+		addr = get_addr(scriptMetadata["Address"])
+		name = scriptMetadata["Name"].encode("utf-8")
+		set_name(addr, name)
+		setEOLComment(addr, name)
+
+if "ScriptMetadataMethod" in data and "ScriptMetadataMethod" in processFields:
+	scriptMetadataMethods = data["ScriptMetadataMethod"]
+	for scriptMetadataMethod in scriptMetadataMethods:
+		addr = get_addr(scriptMetadataMethod["Address"])
+		name = scriptMetadataMethod["Name"].encode("utf-8")
+		methodAddr = get_addr(scriptMetadataMethod["MethodAddress"])
+		set_name(addr, name)
+		setEOLComment(addr, name)
+
+if "Addresses" in data and "Addresses" in processFields:
+	addresses = data["Addresses"]
+	for index in range(len(addresses) - 1):
+		start = get_addr(addresses[index])
+		end = get_addr(addresses[index + 1])
+		make_function(start, end)
+
 print 'Script finished!'
+

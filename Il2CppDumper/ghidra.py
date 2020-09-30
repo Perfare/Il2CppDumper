6cc4de0f6747b8cc26abdb8dd22c9ed031929182
Christian Jones <chjones@aleph0.com>
2020-09-30 18:02:04 -0400

    Initial modification of ghidra.py to apply function signatures/parameters. Unstable. Adapted from @tellowkrinkle's script cited in issue https://github.com/Perfare/Il2CppDumper/issues/366

--- Il2CppDumper/ghidra.py
+++ Il2CppDumper/ghidra.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 import json
 
+from ghidra.app.util.cparser.C import CParserUtils
+from ghidra.app.cmd.function import ApplyFunctionSignatureCmd
+
 processFields = [
 	"ScriptMethod",
 	"ScriptString",
@@ -20,6 +23,24 @@ def set_name(addr, name):
 	name = name.replace(' ', '-')
 	createLabel(addr, name, True, USER_DEFINED)
 
+def set_type(addr, type):
+	# Requires types (il2cpp.h) to be imported first
+	# still shows error dialog for type not found, not ideal, makes unusable
+	# if there are lots
+	#typeSig = CParserUtils.parseSignature(None, currentProgram, type + " a()")
+	#if typeSig is None:
+	#	print("ERROR: Unable to parse type " + type)
+	#else:
+	#	createData(addr, typeSig.getReturnType())
+	newType = type.replace("*"," *").replace("  "," ").strip()
+	dataTypes = getDataTypes(newType)
+	if len(dataTypes) == 0:
+		print("Could not identify type " + type + "(parsed as '" + newType + "')")
+	elif len(dataTypes) > 1:
+		print("Conflicting data types found for type " + type + "(parsed as '" + newType + "')")
+	else:
+		createData(addr, dataTypes[0])
+
 def make_function(start, end):
 	next_func_start = getFunctionAfter(start).getEntryPoint()
 	if next_func_start < end:
@@ -32,6 +53,28 @@ def make_function(start, end):
 		functionManager.createFunction(None, start, body, USER_DEFINED)
 	else:
 		func.setBody(body)
+
+def set_sig(addr, name, sig):
+	# shows error dialog if unable to create (e.g., if types are not found)
+	# These can be numerous. Might be better to find a different way.
+	# Maybe see what parseSignature calls and use that instead?
+	# Should we include the type creation and/or import of header here rather
+	# than to be performed separately? Like ida_with_struct script?
+	# see also warnings in main ghidra console window
+	try: 
+		typeSig = CParserUtils.parseSignature(None, currentProgram, sig, False)
+	except ghidra.app.util.cparser.C.ParseException as e:
+		print("Error: Unable to parse")
+		print(sig)
+		print("Exception type: " + type(e))
+		print("Last eval'd token: " + e.currentToken.toString())
+		print("Error message: " + e.getMessage())
+		raise
+	if typeSig is None:
+		print("Unable to parse function " + name + " with signature '" + sig + "'")
+	else:
+		typeSig.setName(name)
+		ApplyFunctionSignatureCmd(addr, typeSig, USER_DEFINED, False, True).applyTo(currentProgram)
 
 f = askFile("script.json from Il2cppdumper", "Open")
 data = json.loads(open(f.absolutePath, 'rb').read().decode('utf-8'))
@@ -61,6 +104,8 @@ if "ScriptMetadata" in data and "ScriptMetadata" in processFields:
 		name = scriptMetadata["Name"].encode("utf-8")
 		set_name(addr, name)
 		setEOLComment(addr, name)
+		if scriptMetadata["Signature"]:
+			set_type(addr, scriptMetadata["Signature"].encode("utf-8"))
 
 if "ScriptMetadataMethod" in data and "ScriptMetadataMethod" in processFields:
 	scriptMetadataMethods = data["ScriptMetadataMethod"]
@@ -77,6 +122,14 @@ if "Addresses" in data and "Addresses" in processFields:
 		start = get_addr(addresses[index])
 		end = get_addr(addresses[index + 1])
 		make_function(start, end)
+
+if "ScriptMethod" in data and "ScriptMethod" in processFields:
+	scriptMethods = data["ScriptMethod"]
+	for scriptMethod in scriptMethods:
+		addr = get_addr(scriptMethod["Address"])
+		sig = scriptMethod["Signature"][:-1].encode("utf-8")
+		name = scriptMethod["Name"].encode("utf-8")
+		set_sig(addr, name, sig)
 
 print 'Script finished!'
 

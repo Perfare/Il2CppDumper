9f075f9a231d4d45b317714350fa72515bbf4490
Perfare <hjyqwert@126.com>
2023-02-22 13:53:18 +0800

    修复bug

--- Il2CppDumper/Config.cs
+++ Il2CppDumper/Config.cs
@@ -2,20 +2,20 @@
 {
     public class Config
     {
-        public bool DumpMethod = true;
-        public bool DumpField = true;
-        public bool DumpProperty = false;
-        public bool DumpAttribute = false;
-        public bool DumpFieldOffset = true;
-        public bool DumpMethodOffset = true;
-        public bool DumpTypeDefIndex = true;
-        public bool GenerateDummyDll = true;
-        public bool GenerateStruct = true;
-        public bool DummyDllAddToken = true;
-        public bool RequireAnyKey = true;
-        public bool ForceIl2CppVersion = false;
-        public double ForceVersion = 24.3;
-        public bool ForceDump = false;
-        public bool NoRedirectedPointer = false;
+        public bool DumpMethod { get; set; } = true;
+        public bool DumpField { get; set; } = true;
+        public bool DumpProperty { get; set; } = false;
+        public bool DumpAttribute { get; set; } = false;
+        public bool DumpFieldOffset { get; set; } = true;
+        public bool DumpMethodOffset { get; set; } = true;
+        public bool DumpTypeDefIndex { get; set; } = true;
+        public bool GenerateDummyDll { get; set; } = true;
+        public bool GenerateStruct { get; set; } = true;
+        public bool DummyDllAddToken { get; set; } = true;
+        public bool RequireAnyKey { get; set; } = true;
+        public bool ForceIl2CppVersion { get; set; } = false;
+        public double ForceVersion { get; set; } = 24.3;
+        public bool ForceDump { get; set; } = false;
+        public bool NoRedirectedPointer { get; set; } = false;
     }
 }

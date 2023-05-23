e8eadcf1fd9a7a163f07e6251291613305077f4c
AndnixSH <40742924+AndnixSH@users.noreply.github.com>
2023-05-23 21:04:33 +0200

    Update il2cpp_header_to_ghidra.py
    Ask to select il2cpp.h and get absolute path

--- Il2CppDumper/il2cpp_header_to_ghidra.py
+++ Il2CppDumper/il2cpp_header_to_ghidra.py
@@ -1,4 +1,5 @@
-﻿import re
+import re
+import os
 
 header = "typedef unsigned __int8 uint8_t;\n" \
          "typedef unsigned __int16 uint16_t;\n" \
@@ -16,14 +17,15 @@ header = "typedef unsigned __int8 uint8_t;\n" \
 
 def main():
     fixed_header_data = ""
-    with open("il2cpp.h", 'r') as f:
+    headerfile = askFile("il2cpp.h from Il2cppdumper", "Open")
+    with open(headerfile.absolutePath, 'r') as f:
         print("il2cpp.h opened...")
         original_header_data = f.read()
         print("il2cpp.h read...")
         fixed_header_data = re.sub(r": (\w+) {", r"{\n \1 super;", original_header_data)
         print("il2cpp.h data fixed...")
     print("il2cpp.h closed.")
-    with open("il2cpp_ghidra.h", 'w') as f:
+    with open(os.path.join(os.path.dirname(headerfile.absolutePath), "il2cpp_ghidra.h"), 'w') as f:
         print("il2cpp_ghidra.h opened...")
         f.write(header)
         print("header written...")

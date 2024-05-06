59dfd3e5f36ddb8adea565f49ee8b00e25eba9a1
nekomoyi <funny_mo_yi@foxmail.com>
2024-05-06 20:33:30 +0800

    show progress while processing

--- Il2CppDumper/ida_with_struct_py3.py
+++ Il2CppDumper/ida_with_struct_py3.py
@@ -28,6 +28,11 @@ def make_function(start, end):
 		ida_funcs.del_func(start)
 	ida_funcs.add_func(start, end)
 
+def show_progress(field, current, total):
+    if current % 100 == 0:
+        msg = f"Processing {field} {current}/{total} {current * 100 / total:.2f}%"
+        idaapi.replace_wait_box(msg)
+
 path = idaapi.ask_file(False, '*.json', 'script.json from Il2cppdumper')
 hpath = idaapi.ask_file(False, '*.h', 'il2cpp.h from Il2cppdumper')
 parse_decls(open(hpath, 'r').read(), 0)
@@ -36,13 +41,15 @@ data = json.loads(open(path, 'rb').read().decode('utf-8'))
 if "Addresses" in data and "Addresses" in processFields:
 	addresses = data["Addresses"]
 	for index in range(len(addresses) - 1):
+		show_progress("Addresses", index, len(addresses))
 		start = get_addr(addresses[index])
 		end = get_addr(addresses[index + 1])
 		make_function(start, end)
 
 if "ScriptMethod" in data and "ScriptMethod" in processFields:
 	scriptMethods = data["ScriptMethod"]
-	for scriptMethod in scriptMethods:
+	for (index, scriptMethod) in enumerate(scriptMethods):
+		show_progress("ScriptMethod", index, len(scriptMethods))
 		addr = get_addr(scriptMethod["Address"])
 		name = scriptMethod["Name"]
 		set_name(addr, name)
@@ -53,7 +60,8 @@ if "ScriptMethod" in data and "ScriptMethod" in processFields:
 if "ScriptString" in data and "ScriptString" in processFields:
 	index = 1
 	scriptStrings = data["ScriptString"]
-	for scriptString in scriptStrings:
+	for (index, scriptString) in enumerate(scriptStrings):
+		show_progress("ScriptString", index, len(scriptStrings))
 		addr = get_addr(scriptString["Address"])
 		value = scriptString["Value"]
 		name = "StringLiteral_" + str(index)
@@ -63,7 +71,8 @@ if "ScriptString" in data and "ScriptString" in processFields:
 
 if "ScriptMetadata" in data and "ScriptMetadata" in processFields:
 	scriptMetadatas = data["ScriptMetadata"]
-	for scriptMetadata in scriptMetadatas:
+	for (index, scriptMetadata) in enumerate(scriptMetadatas):
+		show_progress("ScriptMetadata", index, len(scriptMetadatas))
 		addr = get_addr(scriptMetadata["Address"])
 		name = scriptMetadata["Name"]
 		set_name(addr, name)
@@ -75,7 +84,8 @@ if "ScriptMetadata" in data and "ScriptMetadata" in processFields:
 
 if "ScriptMetadataMethod" in data and "ScriptMetadataMethod" in processFields:
 	scriptMetadataMethods = data["ScriptMetadataMethod"]
-	for scriptMetadataMethod in scriptMetadataMethods:
+	for (index, scriptMetadataMethod) in enumerate(scriptMetadataMethods):
+		show_progress("ScriptMetadataMethod", index, len(scriptMetadataMethods))
 		addr = get_addr(scriptMetadataMethod["Address"])
 		name = scriptMetadataMethod["Name"]
 		methodAddr = get_addr(scriptMetadataMethod["MethodAddress"])

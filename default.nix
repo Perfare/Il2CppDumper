d46f0305b75d2ebc272742ecb7f3448a895b15b0
Parsa307 <79400634+Parsa307@users.noreply.github.com>
2025-08-23 18:45:46 +0330

    Revert "Revert "Added support for the nix package manager""

---
+++ default.nix
@@ -0,0 +1,18 @@
+{ fetchFromGitHub, buildDotnetModule, dotnetCorePackages }:
+
+buildDotnetModule rec {
+  pname = "Il2CppDumper";
+  version = "6.7.46";
+
+  src = ./.;
+
+  projectFile = "Il2CppDumper/Il2CppDumper.csproj";
+  nugetDeps = ./deps.json;
+
+  dotnet-sdk = with dotnetCorePackages; combinePackages [
+    sdk_8_0
+  ];
+  dotnet-runtime = with dotnetCorePackages; combinePackages [
+    runtime_8_0
+  ];
+}
